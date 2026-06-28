6901a65ddfc506512bdd74ccb31beb84e8d34a2c
lojasamrock-dot <lojasamrock@gmail.com>
2026-06-27 23:08:53 -0300

    Update Aatrexx.py

--- Aatrexx.py
+++ Aatrexx.py
@@ -2324,7 +2324,7 @@ class DuziaAI:
         pesos = fator_decaimento ** (n_amostras - 1 - indices)
         return pesos / pesos.mean()
 
-    def _treinar_ml_online(self):
+    #def _treinar_ml_online(self):
     def _treinar_ml_online(self):
         """Versão corrigida com prevenção de loop infinito e salvamento condicional"""
     if not ML_DISPONIVEL:
